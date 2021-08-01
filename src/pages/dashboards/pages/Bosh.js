511f12a64fc1ff9685b8eda308b897403fcc80c6
Murod <74758823+MurodjohnKhayitov@users.noreply.github.com>
2021-08-01 19:54:22 +0500

    IT-Tower-Finish
    Tugadi:

--- src/pages/dashboards/pages/Bosh.js
+++ src/pages/dashboards/pages/Bosh.js
@@ -20,32 +20,7 @@ import { BrowserRouter,Link } from "react-router-dom";
 export default function Bosh() {
   return (
     <div>
-      <Navbar
-        className={styles.navbar}
-        style={{ backgroundColor: "#FFFFFF" }}
-        collapseOnSelect
-        expand="lg"
-      >
-        <Container>
-          <Navbar.Brand href="#home">
-            <img src={logo122} style={{width:'75px',height:'75px'}} />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className={styles.selectLanguage}> 
-           
-                <BrowserRouter>
-                  
-                <Nav.Link>  <Link to="/">Home</Link></Nav.Link> 
-                <Nav.Link>  <Link to="/course">Course</Link></Nav.Link> 
-                <Nav.Link>     <Link to="/contact">Contact</Link></Nav.Link>  
-                  
-                </BrowserRouter>
-  
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+   
     </div>
   );
 }
