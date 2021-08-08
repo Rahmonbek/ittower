1d4487edaaf132dd69a776b27f59843a9fb992c5
Murod <74758823+MurodjohnKhayitov@users.noreply.github.com>
2021-08-08 10:28:52 +0500

    6u
    u6

--- src/pages/dashboards/pages/card/About.js
+++ src/pages/dashboards/pages/card/About.js
@@ -11,7 +11,7 @@ export default class About extends Component {
     return (
       <div>
         <BrowserRouter>
-          <Bosh4 />
+            <Bosh4 />
           <Switch>
             <Route exact path="/design">
               <Biznes />
